c389aa3917c7715c420189c9c09b785e14493823
richardemslie <richard@clue.co.za>
2013-01-03 01:36:53 +0200

    zxing jars - pending qr scan

---
+++ location/src/nomilous/app2/ScanActivity.java
@@ -0,0 +1,16 @@
+package nomilous.app2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScanActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+
+    }
+
+}
